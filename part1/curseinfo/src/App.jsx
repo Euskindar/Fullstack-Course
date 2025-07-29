a13a468eff1f6bd72f185a81a283e2edde0fcf51
Euskindar <julenjph@gmail.com>
2025-07-29 12:36:03 +0200

    ex 1.4 done

--- part1/curseinfo/src/App.jsx
+++ part1/curseinfo/src/App.jsx
@@ -14,7 +14,7 @@ const Content = ({ parts }) => {
         (element, index) =>
           console.log("Content element: ", element) || ( // Logging each part
             <p key={index}>
-              {element.part}: {element.exercises}
+              {element.name}: {element.exercises}
             </p>
           )
       )}
@@ -37,35 +37,29 @@ const Total = ({ exercises1, exercises2, exercises3 }) => {
 const App = () => {
   const course = "Half Stack application development";
 
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <>
       <Header course={course} />
-      <Content
-        parts={[
-          { part: part1.name, exercises: part1.exercises },
-          { part: part2.name, exercises: part2.exercises },
-          { part: part3.name, exercises: part3.exercises },
-        ]}
-      />
+      <Content parts={parts} />
       <Total
-        exercises1={part1.exercises}
-        exercises2={part2.exercises}
-        exercises3={part3.exercises}
+        exercises1={parts[0].exercises}
+        exercises2={parts[1].exercises}
+        exercises3={parts[2].exercises}
       />
     </>
   );
